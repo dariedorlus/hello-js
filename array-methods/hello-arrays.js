ef2b8fb607656e82d052b992bf89c1d6033fe399
dariedorlus <5068919+dariedorlus@users.noreply.github.com>
2021-09-21 22:29:01 -0400

    Adding  with push

---
+++ array-methods/hello-arrays.js
@@ -0,0 +1,13 @@
+
+
+
+
+//Add the numbers of an array
+
+let array = ["mango", "dorian", "loquat", "jackfruit", "spanish lime", "water melon", "cotton candy grappe"];
+
+console.log(array);
+
+//Add another fruit with push
+array.push("lemon");
+console.log(array);
